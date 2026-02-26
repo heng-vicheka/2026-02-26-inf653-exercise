0d666d89fff74dfc1e69ddf99bd1e5e62e72427f
PK <2023412heng@aupp.edu.kh>
2026-02-26 14:47:16 +0700

    Itemhandlebars

--- app.js
+++ app.js
@@ -1,14 +1,16 @@
 const express = require('express');
 const { engine } = require('express-handlebars');
 const path = require('path');
+const fs = require('fs');
 
 const app = express();
 const PORT = 3000;
+const DATA_FILE = path.join(__dirname, 'datae.json');
 
 app.engine('handlebars', engine({
-  defaultLayout: 'main',              
-  extname: '.handlebars',             
-  partialsDir: ['views/partials/'], 
+  defaultLayout: 'main',
+  extname: '.handlebars',
+  partialsDir: ['views/partials/'],
   helpers: {
     section: function(name, options) {
       if (!this._sections) this._sections = {};
@@ -21,8 +23,51 @@ app.engine('handlebars', engine({
 app.set('view engine', 'handlebars');
 app.set('views', path.join(__dirname, 'views'));
 
-app.use((req, res) => {
-  res.status(404);
+app.use(express.urlencoded({ extended: false }));
+app.use(express.static(path.join(__dirname, 'public')));
+
+function readItems() {
+  return JSON.parse(fs.readFileSync(DATA_FILE, 'utf8'));
+}
+
+function writeItems(items) {
+  fs.writeFileSync(DATA_FILE, JSON.stringify(items, null, 4));
+}
+
+// Dashboard
+app.get('/', (_req, res) => {
+  const items = readItems();
+  res.render('index', { items });
+});
+
+// Item detail
+app.get('/items/:id', (req, res) => {
+  const items = readItems();
+  const item = items.find(i => i.id === req.params.id);
+  if (!item) return res.status(404).send('Item not found');
+  res.render('detail', { item });
+});
+
+// Update status
+app.post('/items/:id/status', (req, res) => {
+  const items = readItems();
+  const item = items.find(i => i.id === req.params.id);
+  if (!item) return res.status(404).send('Item not found');
+  item.status = req.body.status;
+  writeItems(items);
+  res.redirect(`/items/${req.params.id}`);
+});
+
+// Delete item
+app.post('/items/:id/delete', (req, res) => {
+  const items = readItems();
+  const filtered = items.filter(i => i.id !== req.params.id);
+  writeItems(filtered);
+  res.redirect('/');
+});
+
+app.use((_req, res) => {
+  res.status(404).send('Page not found');
 });
 
 app.listen(PORT, () => {
